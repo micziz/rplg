52912ada142050d1b3b03184862a06caa00b4b8d
micziz <miczicontent@gmail.com>
2023-05-05 13:09:47 +0200

    Add more data

--- data.js
+++ data.js
@@ -1,1 +1,1 @@
-module.exports = ["C", "C++", "C#", "Python", "Javascript", "Java", "Typescript", "HTML", "CSS", "Kotlin", "Swift", "Dart"]
+module.exports = ["C", "C++", "C#", "Python", "Javascript", "Java", "Typescript", "HTML", "CSS", "Kotlin", "Swift", "Dart", "V", "Zig", "Nim", "Cobol", "Fortran", "Rust", "Go", "Ada", "Ruby", "PHP"]
